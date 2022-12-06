352ab5f4a335c7125b8987169c24ef5dd30f5395
Giri Priyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2022-12-06 13:04:47 +0530

    Classes

--- app/src/main/java/com/giripriyadarshan/towwn/MainActivity.kt
+++ app/src/main/java/com/giripriyadarshan/towwn/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.giripriyadarshan.towwn.R
+import com.google.android.material.snackbar.Snackbar
 import com.hbb20.CountryCodePicker
 
 
@@ -24,13 +24,58 @@ class MainActivity : AppCompatActivity() {
             val valid = ccpNumberInput.isValidFullNumber
             if (valid) {
                 val phoneNumber = ccpNumberInput.fullNumber
-                val baseUrl = "https://api.whatsapp.com/send/?phone="
-                val suffixUrl = "&text&app_absent=0"
-                val url = baseUrl + phoneNumber + suffixUrl
-                val i = Intent(Intent.ACTION_VIEW)
-                i.data = Uri.parse(url)
-                startActivity(i)
+                openWhatsappContact(phoneNumber)
+
+//                    val baseUrl = "https://api.whatsapp.com/send/?phone="
+//                    val suffixUrl = "&text&app_absent=0"
+//                    val url = baseUrl + phoneNumber + suffixUrl
+//                    val i = Intent(Intent.ACTION_VIEW)
+//                    i.data = Uri.parse(url)
+//                    startActivity(i)
+
+            } else {
+                Snackbar.make(
+                    findViewById(R.id.layout_MainPage),
+                    "Please enter a valid phone number",
+                    Snackbar.LENGTH_LONG
+                ).show()
             }
         }
+    }
+
+    private fun openWhatsappContact(number: String) {
+        val uri = Uri.parse("smsto:$number")
+//        val sendIntent = Intent().apply {
+//            action = Intent.ACTION_SEND_MULTIPLE
+//            putParcelableArrayListExtra(Intent.EXTRA_STREAM, arrayListOf<Uri>(uri))
+//            `package` = "com.whatsapp.w4b"
+//            putExtra("sms_body", "Hello")
+//            putExtra("chat", true)
+//        }
+//        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+//        sendIntent.type = "text/plain";
+//        sendIntent.setPackage("com.whatsapp.w4b");
+
+//        val baseUrl = "https://api.whatsapp.com/send/?phone="
+//                    val suffixUrl = "&text&app_absent=0"
+//                    val url = baseUrl + number + suffixUrl
+//        sendIntent.data = Uri.parse(url)
+
+        val sendIntent = Intent(Intent.ACTION_SENDTO, uri)
+
+        when (val packageName = WhatsappPackage().isWhatsappInstalled(this.packageManager)) {
+            null -> {
+                Snackbar.make(
+                    findViewById(android.R.id.content),
+                    "WhatsApp is not installed on your device",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+            else -> {
+                sendIntent.setPackage(packageName)
+            }
+        }
+
+        startActivity(Intent.createChooser(sendIntent, ""))
     }
 }
